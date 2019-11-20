ff7baa4d8f7ca6110283f3c505e9d6715089b049
Eva Rivarola <melishog@gmail.com>
2019-11-20 17:39:49 -0600

    Se hizo la creación del calendario con las vistas para Mes, semana y día en conjunto. Hay un problema en el popup "opciones".

--- Borrador1/calendario.js
+++ Borrador1/calendario.js
@@ -6,3 +6,23 @@ calendar.theme = "light";
 
 calendar.monthRangeSettings.headerStyle = p.MainHeaderStyle.Title;
 //Object.defineProperty(MonthRangeSettings.prototype, "headerStyle", { value: MainHeaderStyle });
+
+function change_day(){
+  calendar.currentView = p.CalendarView.Timetable;
+  calendar.timetableSettings.headerStyle = p.MainHeaderStyle.Title;
+  calendar.timetableSettings.orientation = p.Orientation.Vertical;
+  calendar.timetableSettings.titleFormat = calendar.formatInfo.dateFormats.longDate;
+  calendar.timetableSettings.cellSize = 60;
+}
+
+function change_week(){
+  calendar.currentView = p.CalendarView.WeekRange;
+  calendar.weekRangeSettings.weekStyle = p.WeekRangeViewStyle.SingleWeek;
+  calendar.weekRangeSettings.headerStyle = p.HorizontalHeaderStyle;
+  calendar.weekRangeSettings.visibleRows = 1;
+  calendar.weekRangeSettings.dayOfWeekFormat = p.DayOfWeekFormat.SingleLetter;
+}
+
+function change_month(){
+  calendar.monthRangeSettings.headerStyle = p.MainHeaderStyle.Title;
+}
